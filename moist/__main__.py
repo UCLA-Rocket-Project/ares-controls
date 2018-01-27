d0e27d9d8bd44ea00462680d73095bf71b53808d
Akhil Palla <akhil@theakhil.com>
2018-01-27 05:47:40 +0000

    Update stuff

--- moist/__main__.py
+++ moist/__main__.py
@@ -4,7 +4,7 @@ from multiprocessing import Process, SimpleQueue
 import serverClient as serv
 import socket
 from libares.moistconsts import *
-from libares.opcodes import *
+from libares.constants import *
 
 dataQueue = SimpleQueue()
 
@@ -28,7 +28,7 @@ def switch_callback(channel):
     switch = MAP[channel]
 
     print("\nevent> channel:", channel, "input:", switch_in)
-    print("event-english> ", OPCODE_STRINGS[switch.opcode],
+    print("event-english> ", opcodes[switch.opcode][op_POS_STR],
         " is being ", ("OPENED" if switch_in else "CLOSED"))
 
     output = (1 if (switch.is_no != switch_in) else 0)
@@ -37,17 +37,21 @@ def switch_callback(channel):
 
 GPIO.setmode(GPIO.BCM)
 
+def resetLEDS(enable=True):
+    for channel, switch in MAP.items():
+        switch_in = getSwitch(channel)
+        if enable:
+            GPIO.output(LEDMAP[switch], switch_in != switch.is_no)
+        else:
+            GPIO.output(LEDMAP[switch], 0)
+
 for pin in MAP:
     GPIO.setup(pin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
     GPIO.add_event_detect(pin, GPIO.BOTH, callback=switch_callback, bouncetime=25)
 
-for valve, ledpin in LEDMAP.items():
-    GPIO.setup(ledpin, GPIO.OUT)
-    GPIO.output(ledpin, 0)
-
 for channel, switch in MAP.items():
     switch_in = getSwitch(channel)
-    GPIO.output(LEDMAP[switch], switch_in)
+    GPIO.setup(LEDMAP[switch], GPIO.OUT)
     sendData(switch.opcode, [1 if (switch.is_no != switch_in) else 0])
 
 GPIO.setup(ENABLE_PIN, GPIO.IN, pull_up_down = GPIO.PUD_UP)
@@ -66,6 +70,7 @@ except socket.error as e:
     usingServer = False
 
 led = 0
+resetLEDS(enable=moistEnabled)
 
 while True:
     value = getSwitch(ENABLE_PIN)
